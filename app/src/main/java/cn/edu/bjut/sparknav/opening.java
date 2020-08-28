733f41c6e85aa0116c63e7bb851e2c3e970b888a
Souredfish <40930586+Souredfish@users.noreply.github.com>
2020-08-28 20:35:03 +0800

    demo

---
+++ app/src/main/java/cn/edu/bjut/sparknav/opening.java
@@ -0,0 +1,14 @@
+package cn.edu.bjut.sparknav;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class opening extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_opening);
+    }
+}
